e4914ea810d0e0cb6e6dad0f6cd8afae4ffebe0d
noxgle <hkon@noxgle>
2020-07-17 20:56:54 +0200

    Add "restart nginx".

--- install_service.bash
+++ install_service.bash
@@ -10,6 +10,7 @@ systemctl enable pidaemon
 cp api_pidaemon.nginx /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/api_pidaemon.nginx /etc/nginx/sites-enabled/
 cp api_pidaemon.service /etc/systemd/system/
+systemctl restart nginx
 systemctl start api_pidaemon
 systemctl enable api_pidaemon
 
